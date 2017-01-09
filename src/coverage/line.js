d09bb90427b80f379206b3f2f490072b5ba6460a
Cédric Belin <cedric@belin.io>
2017-01-09 23:02:11 +0100

    Added the `LineData` class

--- src/coverage/line.js
+++ src/coverage/line.js
@@ -1,3 +1,5 @@
+import {Token} from '../token';
+
 /**
  * Provides the coverage data of lines.
  */
@@ -23,5 +25,56 @@ export class LineData {
    */
   constructor(options = {}) {
 
+    /**
+     * The branch number.
+     * @type {string}
+     */
+    this.checksum = typeof options.checksum == 'string' ? options.checksum : '';
+
+    /**
+     * The block number.
+     * @type {number}
+     */
+    this.executionCount = typeof options.executionCount == 'number' ? options.executionCount : 0;
+
+    /**
+     * The line number.
+     * @type {number}
+     */
+    this.lineNumber = typeof options.lineNumber == 'number' ? options.lineNumber : 0;
+  }
+
+  /**
+   * Creates a new branch data from the specified JSON map.
+   * @param {object} map A JSON map representing a branch data.
+   * @return {BranchData} The instance corresponding to the specified JSON map, or `null` if a parsing error occurred.
+   */
+  static fromJSON(map) {
+    return !map || typeof map != 'object' ? null : new LineData({
+      checksum: map.checksum,
+      executionCount: map.hit,
+      lineNumber: map.line
+    });
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return {object} The map in JSON format corresponding to this object.
+   */
+  toJSON() {
+    return {
+      checksum: this.checksum,
+      hit: this.executionCount,
+      line: this.lineNumber
+    }
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return {string} The string representation of this object.
+   */
+  toString() {
+    let value = `${Token.LINE_DATA}:${this.lineNumber},${this.executionCount}`;
+    return this.checksum.length ? `${value},${this.checksum}` : value;
   }
 }
