0650c342add57e4454500d94ccd0c06f556dd98e
Cédric Belin <cedric@belin.io>
2017-01-09 23:02:02 +0100

    Added the `FunctionData` class

--- src/coverage/function.js
+++ src/coverage/function.js
@@ -1,3 +1,5 @@
+import {Token} from '../token';
+
 /**
  * Provides the coverage data of functions.
  */
@@ -23,5 +25,47 @@ export class FunctionData {
    */
   constructor(options = {}) {
 
+    /**
+     * The branch number.
+     * @type {number}
+     */
+    this.executionCount = typeof options.executionCount == 'number' ? options.executionCount : 0;
+
+    /**
+     * The block number.
+     * @type {string}
+     */
+    this.functionName = typeof options.functionName == 'string' ? options.functionName : '';
+  }
+
+  /**
+   * Creates a new branch data from the specified JSON map.
+   * @param {object} map A JSON map representing a branch data.
+   * @return {BranchData} The instance corresponding to the specified JSON map, or `null` if a parsing error occurred.
+   */
+  static fromJSON(map) {
+    return !map || typeof map != 'object' ? null : new FunctionData({
+      executionCount: map.hit,
+      functionName: map.name
+    });
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return {object} The map in JSON format corresponding to this object.
+   */
+  toJSON() {
+    return {
+      hit: this.executionCount,
+      name: this.functionName
+    }
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return {string} The string representation of this object.
+   */
+  toString() {
+    return `${Token.FUNCTION_DATA}:${this.executionCount},${this.functionName}`;
   }
 }
