50dc21a32bd4f4ea94572cf60d184536f216dac2
Cédric Belin <cedric@belin.io>
2017-01-18 14:37:04 +0100

    Added new test cases

--- test/branch_test.js
+++ test/branch_test.js
@@ -15,13 +15,83 @@ describe('BranchCoverage', () => {
     it('should initialize the existing properties', () => {
       let data = new BranchData();
       let coverage = new BranchCoverage({data: [data], found: 23, hit: 11});
-      assert.equal(coverage.data, 3);
+
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 1);
+      assert.strictEqual(coverage.data[0], data);
+
       assert.equal(coverage.found, 23);
       assert.equal(coverage.hit, 11);
     });
 
     it('should not create new properties', () => {
       assert.ok(!('foo' in new BranchCoverage({foo: 'bar'})));
+    });
+  });
+
+  /**
+   * @test {BranchCoverage.fromJSON}
+   */
+  describe('.fromJSON()', () => {
+    it('should return a null reference with a non-object value', () => {
+      assert.strictEqual(BranchCoverage.fromJSON('foo'), null);
+    });
+
+    it('should return an instance with default values for an empty map', () => {
+      let coverage = BranchCoverage.fromJSON({});
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 0);
+      assert.equal(coverage.found, 0);
+      assert.equal(coverage.hit, 0);
+    });
+
+    it('should return an initialized instance for a non-empty map', () => {
+      let coverage = BranchCoverage.fromJSON({
+        data: [{lineNumber: 23}],
+        found: 23,
+        hit: 11,
+      });
+
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 1);
+      assert.ok(coverage.data[0] instanceof BranchData);
+
+      assert.equal(coverage.found, 23);
+      assert.equal(coverage.hit, 11);
+    });
+  });
+
+  /**
+   * @test {BranchCoverage#toJSON}
+   */
+  describe('.toJSON()', () => {
+    it('should return a map with default values for a newly created instance', () => {
+      let map = new BranchCoverage().toJSON();
+
+      assert.equal(Object.keys(map).length, 3);
+      assert.ok(Array.isArray(map.data));
+      assert.equal(map.data.length, 0);
+
+      assert.equal(map.found, 0);
+      assert.equal(map.hit, 0);
+    });
+
+    it('should return a non-empty map for an initialized instance', () => {
+      let map = new BranchCoverage({
+        data: [new BranchData()],
+        found: 23,
+        hit: 11
+      }).toJSON();
+
+      assert.equal(Object.keys(map).length, 3);
+      assert.ok(Array.isArray(map.data));
+      assert.equal(map.data.length, 1);
+
+      assert.ok(map.data[0] && typeof map.data[0] == 'object');
+      assert.ok('lineNumber' in map.data[0]);
+
+      assert.equal(map.found, 23);
+      assert.equal(map.hit, 11);
     });
   });
 });
--- test/line_test.js
+++ test/line_test.js
@@ -60,6 +60,40 @@ describe('LineCoverage', () => {
       assert.equal(coverage.hit, 11);
     });
   });
+
+  /**
+   * @test {LineCoverage#toJSON}
+   */
+  describe('.toJSON()', () => {
+    it('should return a map with default values for a newly created instance', () => {
+      let map = new LineCoverage().toJSON();
+
+      assert.equal(Object.keys(map).length, 3);
+      assert.ok(Array.isArray(map.data));
+      assert.equal(map.data.length, 0);
+
+      assert.equal(map.found, 0);
+      assert.equal(map.hit, 0);
+    });
+
+    it('should return a non-empty map for an initialized instance', () => {
+      let map = new LineCoverage({
+        data: [new LineData()],
+        found: 23,
+        hit: 11
+      }).toJSON();
+
+      assert.equal(Object.keys(map).length, 3);
+      assert.ok(Array.isArray(map.data));
+      assert.equal(map.data.length, 1);
+
+      assert.ok(map.data[0] && typeof map.data[0] == 'object');
+      assert.ok('lineNumber' in map.data[0]);
+
+      assert.equal(map.found, 23);
+      assert.equal(map.hit, 11);
+    });
+  });
 });
 
 /**
