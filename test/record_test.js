3577affb0d04f0756be7cd220436132367c406e4
Cédric Belin <cedric@belin.io>
2017-01-18 19:40:48 +0100

    Finalized the unit tests of the `Record` class

--- test/record_test.js
+++ test/record_test.js
@@ -44,9 +44,27 @@ describe('Record', () => {
     });
 
     it('should return an instance with default values for an empty map', () => {
+      let record = Record.fromJSON({});
+      assert.ok(record instanceof Record);
+      assert.strictEqual(record.branches, null);
+      assert.strictEqual(record.functions, null);
+      assert.strictEqual(record.lines, null);
+      assert.equal(record.sourceFile, '');
     });
 
     it('should return an initialized instance for a non-empty map', () => {
+      let record = Record.fromJSON({
+        branches: {},
+        functions: {},
+        lines: {},
+        sourceFile: '/home/cedx/lcov.js'
+      });
+
+      assert.ok(record instanceof Record);
+      assert.ok(record.branches instanceof BranchCoverage);
+      assert.ok(record.functions instanceof FunctionCoverage);
+      assert.ok(record.lines instanceof LineCoverage);
+      assert.equal(record.sourceFile, '/home/cedx/lcov.js');
     });
   });
 
@@ -55,9 +73,27 @@ describe('Record', () => {
    */
   describe('#toJSON()', () => {
     it('should return a map with default values for a newly created instance', () => {
+      let map = new Record().toJSON();
+      assert.equal(Object.keys(map).length, 4);
+      assert.strictEqual(map.branches, null);
+      assert.strictEqual(map.functions, null);
+      assert.strictEqual(map.lines, null);
+      assert.equal(map.sourceFile, '');
     });
 
     it('should return a non-empty map for an initialized instance', () => {
+      let map = new Record({
+        branches: new BranchCoverage(),
+        functions: new FunctionCoverage(),
+        lines: new LineCoverage(),
+        sourceFile: '/home/cedx/lcov.js'
+      }).toJSON();
+
+      assert.equal(Object.keys(map).length, 4);
+      assert.ok(map.branches && typeof map.branches == 'object');
+      assert.ok(map.functions && typeof map.functions == 'object');
+      assert.ok(map.lines && typeof map.lines == 'object');
+      assert.equal(map.sourceFile, '/home/cedx/lcov.js');
     });
   });
 
