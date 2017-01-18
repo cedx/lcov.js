2ace2d2aa3e48e2e6f30437b7f2cb0760c340c21
Cédric Belin <cedric@belin.io>
2017-01-18 13:32:42 +0100

    Added the unit tests of the function classes

--- test/function_test.js
+++ test/function_test.js
@@ -1,42 +1,78 @@
 'use strict';
 
 import assert from 'assert';
-import {BranchCoverage, BranchData} from '../src/index';
+import {FunctionCoverage, FunctionData} from '../src/index';
 
 /**
- * @test {BranchCoverage}
+ * @test {FunctionCoverage}
  */
-describe('BranchCoverage', () => {
+describe('FunctionCoverage', () => {
 
   /**
-   * @test {BranchCoverage#constructor}
+   * @test {FunctionCoverage#constructor}
    */
   describe('#constructor()', () => {
     it('should initialize the existing properties', () => {
-      let data = new BranchData();
-      let coverage = new BranchCoverage({data: [data], found: 23, hit: 11});
-      assert.equal(coverage.data, 3);
+      let data = new FunctionData();
+      let coverage = new FunctionCoverage({data: [data], found: 23, hit: 11});
+
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 1);
+      assert.strictEqual(coverage.data[0], data);
+
       assert.equal(coverage.found, 23);
       assert.equal(coverage.hit, 11);
     });
 
     it('should not create new properties', () => {
-      assert.ok(!('foo' in new BranchCoverage({foo: 'bar'})));
+      assert.ok(!('foo' in new FunctionCoverage({foo: 'bar'})));
+    });
+  });
+
+  /**
+   * @test {FunctionCoverage.fromJSON}
+   */
+  describe('.fromJSON()', () => {
+    it('should return a null reference with a non-object value', () => {
+      assert.strictEqual(FunctionCoverage.fromJSON('foo'), null);
+    });
+
+    it('should return an instance with default values for an empty map', () => {
+      let coverage = FunctionCoverage.fromJSON({});
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 0);
+      assert.equal(coverage.found, 0);
+      assert.equal(coverage.hit, 0);
+    });
+
+    it('should return an initialized instance for a non-empty map', () => {
+      let coverage = FunctionCoverage.fromJSON({
+        data: [{lineNumber: 23}],
+        found: 23,
+        hit: 11,
+      });
+
+      assert.ok(Array.isArray(coverage.data));
+      assert.equal(coverage.data.length, 1);
+      assert.ok(coverage.data[0] instanceof FunctionData);
+
+      assert.equal(coverage.found, 23);
+      assert.equal(coverage.hit, 11);
     });
   });
 });
 
 /**
- * @test {BranchData}
+ * @test {FunctionData}
  */
-describe('BranchData', () => {
+describe('FunctionData', () => {
 
   /**
-   * @test {BranchData#constructor}
+   * @test {FunctionData#constructor}
    */
   describe('#constructor()', () => {
     it('should initialize the existing properties', () => {
-      let data = new BranchData({blockNumber: 3, branchNumber: 2, lineNumber: 127, taken: 1});
+      let data = new FunctionData({blockNumber: 3, branchNumber: 2, lineNumber: 127, taken: 1});
       assert.equal(data.blockNumber, 3);
       assert.equal(data.branchNumber, 2);
       assert.equal(data.lineNumber, 127);
@@ -44,20 +80,20 @@ describe('BranchData', () => {
     });
 
     it('should not create new properties', () => {
-      assert.ok(!('foo' in new BranchData({foo: 'bar'})));
+      assert.ok(!('foo' in new FunctionData({foo: 'bar'})));
     });
   });
 
   /**
-   * @test {BranchData.fromJSON}
+   * @test {FunctionData.fromJSON}
    */
   describe('.fromJSON()', () => {
     it('should return a null reference with a non-object value', () => {
-      assert.strictEqual(BranchData.fromJSON('foo'), null);
+      assert.strictEqual(FunctionData.fromJSON('foo'), null);
     });
 
     it('should return an instance with default values for an empty map', () => {
-      let data = BranchData.fromJSON({});
+      let data = FunctionData.fromJSON({});
       assert.equal(data.blockNumber, 0);
       assert.equal(data.branchNumber, 0);
       assert.equal(data.lineNumber, 0);
@@ -65,7 +101,7 @@ describe('BranchData', () => {
     });
 
     it('should return an initialized instance for a non-empty map', () => {
-      let data = BranchData.fromJSON({
+      let data = FunctionData.fromJSON({
         blockNumber: 3,
         branchNumber: 2,
         lineNumber: 127,
@@ -80,11 +116,11 @@ describe('BranchData', () => {
   });
 
   /**
-   * @test {BranchData#toJSON}
+   * @test {FunctionData#toJSON}
    */
   describe('.toJSON()', () => {
     it('should return a map with default values for a newly created instance', () => {
-      let map = new BranchData().toJSON();
+      let map = new FunctionData().toJSON();
       assert.equal(Object.keys(map).length, 4);
       assert.equal(map.blockNumber, 0);
       assert.equal(map.branchNumber, 0);
@@ -93,7 +129,7 @@ describe('BranchData', () => {
     });
 
     it('should return a non-empty map for an initialized instance', () => {
-      let map = new BranchData({
+      let map = new FunctionData({
         blockNumber: 3,
         branchNumber: 2,
         lineNumber: 127,
@@ -109,14 +145,14 @@ describe('BranchData', () => {
   });
 
   /**
-   * @test {BranchData#toString}
+   * @test {FunctionData#toString}
    */
   describe('.toString()', () => {
     it('should return a format like "BRDA:<lineNumber>,<blockNumber>,<branchNumber>,<taken>"', () => {
-      let data = new BranchData();
+      let data = new FunctionData();
       assert.equal(String(data), 'BRDA:0,0,0,-');
 
-      data = new BranchData({blockNumber: 3, branchNumber: 2, lineNumber: 127, taken: 1});
+      data = new FunctionData({blockNumber: 3, branchNumber: 2, lineNumber: 127, taken: 1});
       assert.equal(String(data), 'BRDA:127,3,2,1');
     });
   });
