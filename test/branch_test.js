65f4e8e5ec16941fb2ec4046cb67c0432ccce222
Cédric Belin <cedric@belin.io>
2017-03-10 09:49:58 +0100

    Added new test cases

--- test/branch_test.js
+++ test/branch_test.js
@@ -60,7 +60,7 @@ describe('BranchCoverage', () => {
       expect(Object.keys(map)).to.have.lengthOf(3);
 
       expect(map.data).to.be.an('array').and.have.lengthOf(1);
-      expect(map.data[0]).to.be.an('object').and.have.property('lineNumber');
+      expect(map.data[0]).to.be.an('object').and.have.property('lineNumber').that.is.a('number');
 
       expect(map.found).to.equal(23);
       expect(map.hit).to.equal(11);
