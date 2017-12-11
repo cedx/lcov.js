698c0cb2aa9ac26bb7827f8481db30a48f1dbe7c
Cédric Belin <cedric@belin.io>
2017-12-11 01:41:15 +0100

    Merged some modules

--- test/branch.test.js
+++ test/branch.test.js
@@ -79,3 +79,75 @@ describe('BranchCoverage', () => {
     });
   });
 });
+
+/**
+ * @test {BranchData}
+ */
+describe('BranchData', () => {
+
+  /**
+   * @test {BranchData.fromJson}
+   */
+  describe('.fromJson()', () => {
+    it('should return a null reference with a non-object value', () => {
+      expect(BranchData.fromJson('foo')).to.be.null;
+    });
+
+    it('should return an instance with default values for an empty map', () => {
+      let data = BranchData.fromJson({});
+      expect(data).to.be.instanceof(BranchData);
+      expect(data.blockNumber).to.equal(0);
+      expect(data.branchNumber).to.equal(0);
+      expect(data.lineNumber).to.equal(0);
+      expect(data.taken).to.equal(0);
+    });
+
+    it('should return an initialized instance for a non-empty map', () => {
+      let data = BranchData.fromJson({
+        blockNumber: 3,
+        branchNumber: 2,
+        lineNumber: 127,
+        taken: 1
+      });
+
+      expect(data).to.be.instanceof(BranchData);
+      expect(data.blockNumber).to.equal(3);
+      expect(data.branchNumber).to.equal(2);
+      expect(data.lineNumber).to.equal(127);
+      expect(data.taken).to.equal(1);
+    });
+  });
+
+  /**
+   * @test {BranchData#toJSON}
+   */
+  describe('#toJSON()', () => {
+    it('should return a map with default values for a newly created instance', () => {
+      let map = new BranchData(0, 0, 0).toJSON();
+      expect(Object.keys(map)).to.have.lengthOf(4);
+      expect(map.blockNumber).to.equal(0);
+      expect(map.branchNumber).to.equal(0);
+      expect(map.lineNumber).to.equal(0);
+      expect(map.taken).to.equal(0);
+    });
+
+    it('should return a non-empty map for an initialized instance', () => {
+      let map = new BranchData(127, 3, 2, 1).toJSON();
+      expect(Object.keys(map)).to.have.lengthOf(4);
+      expect(map.blockNumber).to.equal(3);
+      expect(map.branchNumber).to.equal(2);
+      expect(map.lineNumber).to.equal(127);
+      expect(map.taken).to.equal(1);
+    });
+  });
+
+  /**
+   * @test {BranchData#toString}
+   */
+  describe('#toString()', () => {
+    it('should return a format like "BRDA:<lineNumber>,<blockNumber>,<branchNumber>,<taken>"', () => {
+      expect(String(new BranchData(0, 0, 0))).to.equal('BRDA:0,0,0,-');
+      expect(String(new BranchData(127, 3, 2, 1))).to.equal('BRDA:127,3,2,1');
+    });
+  });
+});
--- test/function.test.js
+++ test/function.test.js
@@ -79,3 +79,75 @@ describe('FunctionCoverage', () => {
     });
   });
 });
+
+/**
+ * @test {FunctionData}
+ */
+describe('FunctionData', () => {
+
+  /**
+   * @test {FunctionData.fromJson}
+   */
+  describe('.fromJson()', () => {
+    it('should return a null reference with a non-object value', () => {
+      expect(FunctionData.fromJson('foo')).to.be.null;
+    });
+
+    it('should return an instance with default values for an empty map', () => {
+      let data = FunctionData.fromJson({});
+      expect(data).to.be.instanceof(FunctionData);
+      expect(data.executionCount).to.equal(0);
+      expect(data.functionName).to.be.empty;
+      expect(data.lineNumber).to.equal(0);
+    });
+
+    it('should return an initialized instance for a non-empty map', () => {
+      let data = FunctionData.fromJson({
+        executionCount: 3,
+        functionName: 'main',
+        lineNumber: 127
+      });
+
+      expect(data).to.be.instanceof(FunctionData);
+      expect(data.executionCount).to.equal(3);
+      expect(data.functionName).to.equal('main');
+      expect(data.lineNumber).to.equal(127);
+    });
+  });
+
+  /**
+   * @test {FunctionData#toJSON}
+   */
+  describe('#toJSON()', () => {
+    it('should return a map with default values for a newly created instance', () => {
+      let map = new FunctionData('', 0).toJSON();
+      expect(Object.keys(map)).to.have.lengthOf(3);
+      expect(map.executionCount).to.equal(0);
+      expect(map.functionName).to.be.empty;
+      expect(map.lineNumber).to.equal(0);
+    });
+
+    it('should return a non-empty map for an initialized instance', () => {
+      let map = new FunctionData('main', 127, 3).toJSON();
+      expect(Object.keys(map)).to.have.lengthOf(3);
+      expect(map.executionCount).to.equal(3);
+      expect(map.functionName).to.equal('main');
+      expect(map.lineNumber).to.equal(127);
+    });
+  });
+
+  /**
+   * @test {FunctionData#toString}
+   */
+  describe('#toString()', () => {
+    it('should return a format like "FN:<lineNumber>,<functionName>" when used as definition', () => {
+      expect(new FunctionData('', 0).toString(true)).to.equal('FN:0,');
+      expect(new FunctionData('main', 127, 3).toString(true)).to.equal('FN:127,main');
+    });
+
+    it('should return a format like "FNDA:<executionCount>,<functionName>" when used as data', () => {
+      expect(new FunctionData('', 0).toString(false)).to.equal('FNDA:0,');
+      expect(new FunctionData('main', 127, 3).toString(false)).to.equal('FNDA:3,main');
+    });
+  });
+});
