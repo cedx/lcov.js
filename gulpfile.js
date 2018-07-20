a53c9f0793732c4f78e35ae1faf79ac2326e4683
Cédric Belin <cedric@belin.io>
2018-07-20 20:58:12 +0200

    Added a 'fix:security' build task

--- gulpfile.js
+++ gulpfile.js
@@ -34,10 +34,13 @@ gulp.task('doc', gulp.series('doc:api', 'doc:web'));
 /**
  * Fixes the coding standards issues.
  */
-gulp.task('fix', () => gulp.src(['*.js', 'example/*.js', 'lib/**/*.js', 'test/**/*.js'], {base: '.'})
+gulp.task('fix:js', () => gulp.src(['*.js', 'example/*.js', 'lib/**/*.js', 'test/**/*.js'], {base: '.'})
   .pipe(eslint({fix: true}))
   .pipe(gulp.dest('.'))
 );
+
+gulp.task('fix:security', () => _exec('npm', ['audit', 'fix']));
+gulp.task('fix', gulp.series('fix:js', 'fix:security'));
 
 /**
  * Performs static analysis of source code.
