bada3d8667ea1b74d752d8ea892619ad65d173ee
Cédric Belin <cedric@belin.io>
2018-08-10 15:53:33 +0200

    Updated the documentation

--- src/token.ts
+++ src/token.ts
@@ -1,32 +1,70 @@
 /**
  * Provides the list of tokens supported by the parser.
- *
- * @property branchData The coverage data of a branch.
- * @property branchesFound The number of branches found.
- * @property branchesHit The number of branches hit.
- * @property endOfRecord The end of a section.
- * @property functionData The coverage data of a function.
- * @property functionName A function name.
- * @property functionsFound The number of functions found.
- * @property functionsHit The number of functions hit.
- * @property lineData The coverage data of a line.
- * @property linesFound The number of lines found.
- * @property linesHit The number of lines hit.
- * @property sourceFile The path to a source file.
- * @property testName A test name.
  */
 export enum Token {
+
+  /**
+   * The coverage data of a branch.
+   */
   branchesFound = 'BRF',
+
+  /**
+   * The number of branches found.
+   */
   branchesHit = 'BRH',
+
+  /**
+   * The number of branches hit.
+   */
   branchData = 'BRDA',
+
+  /**
+   * The end of a section.
+   */
   endOfRecord = 'end_of_record',
+
+  /**
+   * The coverage data of a function.
+   */
   functionData = 'FNDA',
+
+  /**
+   * A function name.
+   */
   functionName = 'FN',
+
+  /**
+   * The number of functions found.
+   */
   functionsFound = 'FNF',
+
+  /**
+   * The number of functions hit.
+   */
   functionsHit = 'FNH',
+
+  /**
+   * The coverage data of a line.
+   */
   lineData = 'DA',
+
+  /**
+   * The number of lines found.
+   */
   linesFound = 'LF',
+
+  /**
+   * The number of lines hit.
+   */
   linesHit = 'LH',
+
+  /**
+   * The path to a source file.
+   */
   sourceFile = 'SF',
+
+  /**
+   * A test name.
+   */
   testName = 'TN'
 }
