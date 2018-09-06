68f7e1dce10c58ef31ff2fb3f8e0dc5dedd58709
Cédric Belin <cedric@belin.io>
2018-09-06 14:39:57 +0200

    The map interfaces are not exported anymore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './branch';
 export * from './function';
 export * from './line';
-export * from './map';
 export * from './record';
 export * from './report';
 export * from './token';
