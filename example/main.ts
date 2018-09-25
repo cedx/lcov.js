33f40726fe4e88ed09812c247c532f077b41755d
Cédric Belin <cedric@belin.io>
2018-09-25 22:52:01 +0200

    Fixed the code samples

--- example/main.ts
+++ example/main.ts
@@ -7,23 +7,7 @@ import {promises} from 'fs';
 /**
  * Formats coverage data as LCOV report.
  */
-async function formatReport(): Promise<void> {
-  try {
-    const coverage = await promises.readFile('lcov.info', 'utf8');
-    const report = Report.fromCoverage(coverage);
-    console.log(`The coverage report contains ${report.records.length} records:`);
-    console.log(report.toJSON());
-  }
-
-  catch (error) {
-    console.log(`An error occurred: ${error.message}`);
-  }
-}
-
-/**
- * Parses a LCOV report to coverage data.
- */
-function parseReport(): void {
+function formatReport(): void {
   const lineCoverage = new LineCoverage(2, 2, [
     new LineData(6, 2, 'PF4Rz2r7RTliO9u6bZ7h6g'),
     new LineData(7, 2, 'yGMB6FhEEAd8OyASe3Ni1w')
@@ -36,4 +20,20 @@ function parseReport(): void {
 
   const report = new Report('Example', [record]);
   console.log(report.toString());
+}
+
+/**
+ * Parses a LCOV report to coverage data.
+ */
+async function parseReport(): Promise<void> {
+  try {
+    const coverage = await promises.readFile('lcov.info', 'utf8');
+    const report = Report.fromCoverage(coverage);
+    console.log(`The coverage report contains ${report.records.length} records:`);
+    console.log(report.toJSON());
+  }
+
+  catch (error) {
+    console.log(`An error occurred: ${error.message}`);
+  }
 }
