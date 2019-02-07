591867cae12bc7df1a5d81b33699c200d20c8666
Cédric Belin <cedric@belin.io>
2019-02-07 13:23:18 +0100

    Expose the `map` interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './branch';
 export * from './function';
 export * from './line';
+export * from './map';
 export * from './record';
 export * from './report';
 export * from './token';
