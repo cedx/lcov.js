f1e1620cc48919d9676b4ec56ab3fb9b49cc7b50
Cédric Belin <cedric@belin.io>
2019-04-14 11:56:38 +0200

    Updated the Rollup settings

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,7 +1,8 @@
+import {join} from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: 'lib/index.mjs',
-  output: {file: 'build/lcov.js', format: 'iife', name: 'lcov'},
+  input: join(__dirname, '../lib/index.mjs'),
+  output: {file: join(__dirname, '../build/lcov.js'), format: 'iife', name: 'lcov'},
   plugins: [resolve()]
 };
