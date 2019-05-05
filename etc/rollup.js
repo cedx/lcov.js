63a56e188478753fe3b8c12721b8a19f858258c4
Cédric Belin <cedric@belin.io>
2019-05-05 20:30:32 +0200

    Updated the bundle generation

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,8 +1,6 @@
 import {join} from 'path';
-import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: join(__dirname, '../lib/index.mjs'),
-  output: {file: join(__dirname, '../build/lcov.js'), format: 'iife', name: 'lcov'},
-  plugins: [resolve()]
+  input: join(__dirname, '../lib/index.js'),
+  output: {file: join(__dirname, '../build/lcov.js'), format: 'iife', name: 'lcov'}
 };
