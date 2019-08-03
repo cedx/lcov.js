ae73170f8579dd7deb788f9a0921c25d9101bf6a
Cédric Belin <cedric@belin.io>
2019-08-03 15:54:34 +0200

    Code optimization

--- src/json_map.ts
+++ src/json_map.ts
@@ -1,6 +1,2 @@
 /** Defines the shape of a map in JSON format. */
-export interface JsonMap {
-
-  /** Gets or sets the value for the given key. */
-  [key: string]: any;
-}
+export type JsonMap = Record<string, any>;
