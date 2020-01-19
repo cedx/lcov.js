bc45ec745138f3d82f730544dce67d0a401cf1fd
Cédric Belin <cedric@belin.io>
2020-01-19 20:12:28 +0100

    Added the generated files for the code coverage

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+export * from './branch';
+export * from './function';
+export * from './json';
+export * from './line';
+export * from './record';
+export * from './report';
+export * from './token';
