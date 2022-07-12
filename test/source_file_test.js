8fb4997fcd87fc7fb989abe865cee5a81a292d43
Cédric Belin <cedric@belin.io>
2022-07-12 22:10:44 +0200

    Updated the API reference

--- test/source_file_test.js
+++ test/source_file_test.js
@@ -20,35 +20,6 @@ test("SourceFile.fromJson()", async ctx => {
 	});
 });
 
-test("SourceFile.toJSON()", async ctx => {
-	await ctx.test("should return a map with default values for a newly created instance", () => {
-		const map = new SourceFile("").toJSON();
-		assert.equal(Object.keys(map).length, 4);
-		assert.equal(map.branches, null);
-		assert.equal(map.functions, null);
-		assert.equal(map.lines, null);
-		assert.equal(map.path.length, 0);
-	});
-
-	await ctx.test("should return a non-empty map for an initialized instance", () => {
-		const record = new SourceFile("/home/cedx/lcov.js", {
-			branches: new BranchCoverage,
-			functions: new FunctionCoverage,
-			lines: new LineCoverage
-		});
-
-		const map = record.toJSON();
-		assert.equal(Object.keys(map).length, 4);
-		assert.ok(map.branches);
-		assert.equal(typeof map.branches, "object");
-		assert.ok(map.functions);
-		assert.equal(typeof map.functions, "object");
-		assert.ok(map.lines);
-		assert.equal(typeof map.lines, "object");
-		assert.equal(map.path, "/home/cedx/lcov.js");
-	});
-});
-
 test("SourceFile.toString()", async ctx => {
 	await ctx.test("should return a format like 'SF:<path>\\nend_of_record'", () => {
 		assert.equal(String(new SourceFile("")), "SF:\nend_of_record");
