40bfc2f073676a402da5664136557e6f3a3aee71
Cédric Belin <cedric@belin.io>
2024-01-26 16:13:12 +0100

    Code formatting [skip ci]

--- example/format_report.js
+++ example/format_report.js
@@ -1,9 +1,7 @@
 import console from "node:console";
 import {FunctionCoverage, LineCoverage, LineData, Report, SourceFile} from "@cedx/lcov";
 
-/**
- * Formats coverage data as LCOV report.
- */
+// Formats coverage data as LCOV report.
 const sourceFile = new SourceFile("/home/cedx/lcov.js/fixture.js", {
 	functions: new FunctionCoverage({found: 1, hit: 1}),
 	lines: new LineCoverage({found: 2, hit: 2, data: [
--- example/parse_report.js
+++ example/parse_report.js
@@ -2,9 +2,7 @@ import console from "node:console";
 import {readFile} from "node:fs/promises";
 import {Report} from "@cedx/lcov";
 
-/**
- * Parses a LCOV report to coverage data.
- */
+// Parses a LCOV report to coverage data.
 try {
 	const report = Report.parse(await readFile("/path/to/lcov.info", "utf8"));
 	console.log(`The coverage report contains ${report.sourceFiles.length} source files:`);
