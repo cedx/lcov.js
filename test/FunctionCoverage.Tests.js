4a727836badcfb9fb464cd00dac1328be211c0e7
Cédric Belin <cedx@outlook.com>
2026-04-24 17:15:37 +0200

    Update the tests

--- test/FunctionCoverage.Tests.js
+++ test/FunctionCoverage.Tests.js
@@ -10,8 +10,8 @@ describe("FunctionCoverage", () => {
 		it("should return a format like 'FNF:<found>\\nFNH:<hit>'", () => {
 			equal(String(new FunctionCoverage), "FNF:0\nFNH:0");
 
-			const coverage = new FunctionCoverage({data: [new FunctionData({executionCount: 3, functionName: "main", lineNumber: 127})], found: 23, hit: 11});
-			equal(String(coverage), "FN:127,main\nFNDA:3,main\nFNF:23\nFNH:11");
+			const data = new FunctionData({executionCount: 3, functionName: "main", lineNumber: 127});
+			equal(String(new FunctionCoverage({data: [data], found: 23, hit: 11})), `${data}\nFNF:23\nFNH:11`);
 		});
 	});
 });
